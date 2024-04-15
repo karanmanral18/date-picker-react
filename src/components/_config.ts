e707fe8be32d819e5a8e6ae10a54d71a68be25d3
karan <karanmanral18@gmail.com>
2024-04-15 18:56:55 +0530

    add styling for date picker

---
+++ src/components/_config.ts
@@ -0,0 +1,2 @@
+export const weeks = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+export const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
